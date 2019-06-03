1c0612faf3fb39fadd60487c9c8c4d0ef7032432
auyeskhanm <auyeskhanm@gmail.com>
2019-06-03 21:12:41 +0600

    Добавьте файлы проекта.

---
+++ SaveFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(@"https://nsportal.ru/sites/default/files/2014/01/16/issledovatelskaya_rabota_po_osvoeniyu_kosmosa.docx",
+                    "DocFile.txt");
+            }
+        }
+    }
+}
